934b1505b46ea734e22c255ce7955b77062dc94e
rdotzaz <radek.zaz.1999@gmail.com>
2020-09-14 15:20:18 +0200

    Squad layout has been created. Logic to SquadActivity soon...

---
+++ app/src/main/java/rdotzaz/footnote/SquadActivity.java
@@ -0,0 +1,14 @@
+package rdotzaz.footnote;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SquadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_squad);
+    }
+}
